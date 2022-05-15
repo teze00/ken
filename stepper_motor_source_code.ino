195ef4f7c4ad9c6483d440f32a445b8f01e6e040
teze00 <105608301+teze00@users.noreply.github.com>
2022-05-15 13:39:24 -0700

    Add files via upload

---
+++ stepper_motor_source_code.ino
@@ -0,0 +1,33 @@
+#include<Stepper.h>
+int blue = 12;
+int pink =11;
+int yellow = 10;
+int orange = 9;
+int steps = 2038;
+Stepper stepper (steps,blue,yellow,pink,orange);
+int direct = 1;
+int revTime;
+float secs,revs;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("rpm tim(s) revs");
+}
+void loop(){
+  for (int i = 2; i<19; i=i+2){
+    stepper.setSpeed(i);
+    direct = -direct;
+    revTime = millis();
+    stepper.step(direct* steps/2);
+    revTime = millis()-revTime;
+    delay(500);
+    secs =revTime/1000.0;
+    revs=i*secs/60.0;
+    Serial.print(i);
+    Serial.print("\t");
+    Serial.print(secs);
+    Serial.print("\t");
+    Serial.println(revs,3);
+  }
+}
+
